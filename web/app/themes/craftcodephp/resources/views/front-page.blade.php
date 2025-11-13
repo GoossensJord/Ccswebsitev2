017db3c3965e27a494179bce76ae89690ddf8ddd
Jord Goossens <goossensjord99@gmail.com>
2025-11-13 08:39:17 +0100

    insights section added

--- web/app/themes/craftcodephp/resources/views/front-page.blade.php
+++ web/app/themes/craftcodephp/resources/views/front-page.blade.php
@@ -21,7 +21,7 @@ add_action('wp_enqueue_scripts', 'enqueue_theme_styles');
 @include('sections.collaboration')
 @include('sections.project-showcase')
 @include('sections.company-logo-carousel')
-@include('sections.impact-highlights')
+@include('sections.impact-highlight')
 @include('sections.insights')
 
 
